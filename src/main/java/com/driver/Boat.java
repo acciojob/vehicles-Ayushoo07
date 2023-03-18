3a5eae53fcb7691c320a0b1868e4ac20dfe096e5
unknown <akhileshayush.2016@gmail.com>
2023-03-18 13:35:18 +0530

    Attempt 1

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+public class Boat implements WaterVehicle {
+private String name;
+ private int capcity;
+public Boat(String name,int capcity)
+{
+    this.name = name;
+    this.capcity=capcity;
+}
+@Override public String getVehicleName() {
+    return this.name;
+    }@Override public int getVehicleCapacity() {
+    return this.capcity;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,12 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+    super(name,4,2,6,isManual,"F1",2);
         //Use arbitrary values for parameters which are not mentioned
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = super.getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -19,12 +20,47 @@ public class F1 extends Car {
          */
 
         if(newSpeed == 0) {
+            super.stop();
+            this.changeGear(1);
             //Stop the car, set gear as 1
         }
         //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+            if(newSpeed>=1 && newSpeed<=50)
+            {
+                changeSpeed(newSpeed, super.getCurrentDirection());
+                this.changeGear(1);
+            }
+            else if(newSpeed>=51 && newSpeed<=100)
+            {
+                changeSpeed(newSpeed, super.getCurrentDirection());
+                this.changeGear(2);
+            }
+
+            else if(newSpeed>=101 && newSpeed<=150)
+            {
+                changeSpeed(newSpeed, super.getCurrentDirection());
+                this.changeGear(3);
+            }
+
+            else if(newSpeed>=151 && newSpeed<=200)
+            {
+                changeSpeed(newSpeed, super.getCurrentDirection());
+                this.changeGear(4);
+            }
+
+            else if(newSpeed>=201 && newSpeed<=250)
+            {
+                changeSpeed(newSpeed, super.getCurrentDirection());
+                this.changeGear(5);
+            }
+            else if (newSpeed >250)
+            {
+
+                changeSpeed(newSpeed, super.getCurrentDirection());
+                this.changeGear(6);
+            }
         }
     }
 }
